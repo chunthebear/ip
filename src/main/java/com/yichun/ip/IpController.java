4b65e2c1eb72e4a6c15cbbc726c2f43e963df6a6
chunthebear <zyc5026@hotmail.com>
2020-01-10 13:40:39 -0800

    reference added

--- src/main/java/com/yichun/ip/IpController.java
+++ src/main/java/com/yichun/ip/IpController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IpController {
 	
 	// Sends ping request to a provided IP address 
+	// Codes adapted from https://www.geeksforgeeks.org/pinging-ip-address-java/
 	@RequestMapping("/check")
 	public static Ip sendPingRequest(@RequestParam(value="ip", defaultValue="255.255.255.0") String ipAddress) { 
 		String status = "";
