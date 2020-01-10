8a599077dd77702c07acc9df95c0209d20e01a27
chunthebear <zyc5026@hotmail.com>
2020-01-10 13:56:04 -0800

    spacing formatted

--- src/main/java/com/yichun/ip/IpController.java
+++ src/main/java/com/yichun/ip/IpController.java
@@ -17,21 +17,21 @@ public class IpController {
 	@RequestMapping("/check")
 	public static Ip sendPingRequest(@RequestParam(value="ip", defaultValue="255.255.255.0") String ipAddress) { 
 		String status = "";
-		try{
-	    	InetAddress geek = InetAddress.getByName(ipAddress); 
-		    System.out.println("Sending Ping Request to " + ipAddress); 
-		    if (geek.isReachable(5000)) {
-		    	System.out.println("Host is reachable"); 
-		    	status = "Reachable";
-		    } else {
-		    	System.out.println("Sorry ! We can't reach to this host"); 
-		    	status = "Unreachable";
-		    }
+		try {
+			InetAddress geek = InetAddress.getByName(ipAddress); 
+			System.out.println("Sending Ping Request to " + ipAddress); 
+			if (geek.isReachable(5000)) {
+				System.out.println("Host is reachable"); 
+				status = "Reachable";
+			} else {
+				System.out.println("Sorry ! We can't reach to this host"); 
+				status = "Unreachable";
+			}
 		} catch (UnknownHostException e) {
-	    	e.printStackTrace();
-	    } catch (IOException e) {
-	    	e.printStackTrace();
-	    }
-	    return new Ip(ipAddress, status);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new Ip(ipAddress, status);
 	} 
 }
