fbb4c556e44283d586e983d4d32f0d0fcdbadf75
chunthebear <zyc5026@hotmail.com>
2020-01-10 15:04:56 -0800

    Moved status checking logic from controller to model

--- src/main/java/com/yichun/ip/Ip.java
+++ src/main/java/com/yichun/ip/Ip.java
@@ -1,14 +1,18 @@
 package com.yichun.ip;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Ip {
 
 	private final String ip;
 	private final String status;
 	
-	public Ip(String ip, String status) {
+	public Ip(String ip) {
 		super();
 		this.ip = ip;
-		this.status = status;
+		this.status = checkStatus(ip);
 	}
 	
 	public String getIp() {
@@ -16,6 +20,26 @@ public class Ip {
 	}
 	
 	public String getStatus() {
+		return status;
+	}
+	
+	public static String checkStatus(String ipAddress) { 
+		String status = "";
+		try {
+			InetAddress geek = InetAddress.getByName(ipAddress); 
+			System.out.println("Sending Ping Request to " + ipAddress); 
+			if (geek.isReachable(5000)) {
+				System.out.println("Host is reachable"); 
+				status = "Reachable";
+			} else {
+				System.out.println("Sorry ! We can't reach to this host"); 
+				status = "Unreachable";
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return status;
 	}
 }
--- src/main/java/com/yichun/ip/IpController.java
+++ src/main/java/com/yichun/ip/IpController.java
@@ -1,9 +1,5 @@
 package com.yichun.ip;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,22 +12,6 @@ public class IpController {
 	// Codes adapted from https://www.geeksforgeeks.org/pinging-ip-address-java/
 	@RequestMapping("/check")
 	public static Ip sendPingRequest(@RequestParam(value="ip", defaultValue="255.255.255.0") String ipAddress) { 
-		String status = "";
-		try {
-			InetAddress geek = InetAddress.getByName(ipAddress); 
-			System.out.println("Sending Ping Request to " + ipAddress); 
-			if (geek.isReachable(5000)) {
-				System.out.println("Host is reachable"); 
-				status = "Reachable";
-			} else {
-				System.out.println("Sorry ! We can't reach to this host"); 
-				status = "Unreachable";
-			}
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return new Ip(ipAddress, status);
+		return new Ip(ipAddress);
 	} 
 }
